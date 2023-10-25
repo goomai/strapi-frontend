09c21cc9436b4333b82774f161a47d3fff7297fb
goomai <124039476+goomai@users.noreply.github.com>
2023-10-25 14:43:40 +0530

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -18,12 +18,8 @@ function Terms() {
         <h1>Terms and Conditions</h1>
         <div>
           <p>
-            Throughout the site, the terms “we”, “us” and “our” refer to the
-            website{" "}
-            <Link href="https://goom.ai" rel="noreferrer" className="blue_link">
-              https://goom.ai
-            </Link>
-            . We offer this website, including all information, tools and
+            Throughout the site, the terms “we”, “us” and “our” refer to the Keevs Health India Pvt Ltd.
+            We offer this website, including all information, tools and
             services available from this site to you, the user, conditioned upon
             your acceptance of all terms, conditions, policies and notices
             stated here.
